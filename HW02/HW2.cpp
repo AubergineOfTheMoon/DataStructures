954595ea95fef6be607df60d5c21eca4171726ab
oconnokb <buritobison0@gmail.com>
2017-02-02 19:13:45 -0500

    int main changes
    do while loop

--- HW02/HW2.cpp
+++ HW02/HW2.cpp
@@ -8,4 +8,17 @@ using namespace std;
 int main() 
 {
 	srand(time(NULL));
+	int sides;
+	cout << "Welcome to the game of dice rolling" << endl;
+	do
+	{
+		char play;
+		int sides;
+		cout << "Enter the number of sides you want to roll: ";
+		cin >> sides;
+		if (sides < 6 || sides > 20)
+		{
+			cout << "Must be between 6 and 20";
+		}
+	} while (sides < 6 || sides > 20);
 }
