0c26d68cbf6e4327fac682582d88ad2d68ebfb5b
Saylee <dharnesk@mail.uc.edu>
2017-02-26 19:53:47 -0500

    Changed main function to work for 2 players
    Added conditions for valid moves and conditions to win

--- Lab06 - 2Player/Lab06.cpp
+++ Lab06 - 2Player/Lab06.cpp
@@ -46,28 +46,30 @@ void displayTowers(Stack<Disk> &l, Stack<Disk> &m, Stack<Disk> &r, int size) { /
 	cout << t;
 }
 
+int changePlayer(int playerNum) {
+	if(playerNum == 0){ return 1; }
+	else { return 0; }
+}
+
 int main() {
 	cout << "Welcome to Towers of Hanoi!" << endl << endl;
 	int numDisks;
-	int twoPlayer = 1;
-	// Two Player Mode
-	do {
-		cout << "Would you like to Play 1 player or 2 player? [1/2]" << endl;
-		cin >> twoPlayer;
-	} while ((twoPlayer != 1) && (twoPlayer != 2));
-	// get valid int
-	do {
-		cout << "How many disks would you like to use?: ";
+
+	// Choose number of disks.
+	do { 
+		cout << "Players, how many disks would you like to use?: ";
 		cin >> numDisks;
 		if (numDisks < 1) {
 			cout << "Please choose a positive number of disks" << endl;
 		}
 	} while (numDisks < 1);
-	cout << "Hi" << endl;
+
 	// make 3 stacks, one for each tower
 	Stack<Disk> lTower = Stack<Disk>(numDisks);
 	Stack<Disk> mTower = Stack<Disk>(numDisks);
 	Stack<Disk> rTower = Stack<Disk>(numDisks);
+	// Make tower to check for a win.
+	Stack<Disk> possibleWinTower = Stack<Disk>(numDisks);
 
 	// makes a pointer array of disks, and pushes it to the left tower
 	Disk *disks = new Disk[numDisks];
@@ -77,24 +79,56 @@ int main() {
 		disks[i - 1].color = "Blue";
 		lTower.push(&disks[i - 1]); // push the disk to the left tower
 	}
-	if (twoPlayer == 2) {
-		for (int i = numDisks; i > 0; i--) {
-			disks2[i - 1].diskNum = i;
-			disks2[i - 1].color = "Red ";
-			rTower.push(&disks2[i - 1]);
-		}
+	for (int i = numDisks; i > 0; i--) {
+		disks2[i - 1].diskNum = i;
+		disks2[i - 1].color = "Red ";
+		rTower.push(&disks2[i - 1]);
 	}
 
 	bool stillPlaying = true;
 	int tower;
 	Stack<Disk>* fromTower; // the tower the disk comes from
 	Stack<Disk>* toTower; // the tower the disk goes
-
+	string playerColors[] = {"Blue", "Red "};
+	Stack<Disk>* winningTowers[2] = {&rTower, &lTower};
+	string winner;
+	int currentPlayer = 0;
+	bool invalidTower = false;
 	while (stillPlaying) {
+
 		cout << endl;
+		cout << "Player " << currentPlayer + 1 << " is playing. Their color is " << playerColors[currentPlayer] << endl;
 		displayTowers(lTower, mTower, rTower, numDisks);
+		
+		// Check if any move is available for the player
+		if (!lTower.isEmpty() && !mTower.isEmpty() && rTower.isEmpty()) {
+			if ((lTower.top()->color != playerColors[currentPlayer]) && (mTower.top()->color != playerColors[currentPlayer])) {
+				cout << "You have no possible moves." << endl;
+				continue;
+			}
+		}
+		else if (!lTower.isEmpty() && mTower.isEmpty() && !rTower.isEmpty()) {
+			if ((lTower.top()->color != playerColors[currentPlayer]) && (rTower.top()->color != playerColors[currentPlayer])) {
+				cout << "You have no possible moves." << endl;
+				continue;
+			}
+		}
+		else if (lTower.isEmpty() && !mTower.isEmpty() && !rTower.isEmpty()) {
+			if ((mTower.top()->color != playerColors[currentPlayer]) && (rTower.top()->color != playerColors[currentPlayer])) {
+				cout << "You have no possible moves." << endl;
+				continue;
+			}
+		}
+		else if (!lTower.isEmpty() && !mTower.isEmpty() && !rTower.isEmpty()) {
+			if ((lTower.top()->color != playerColors[currentPlayer]) && (mTower.top()->color != playerColors[currentPlayer]) && (rTower.top()->color != playerColors[currentPlayer])) {
+				cout << "You have no possible moves." << endl;
+				continue;
+			}
+		}
+		
+		// Pick up the disk
 		do {
-			// Pick up the disk
+			invalidTower = false;
 			cout << "Choose the tower you would like to pick from: ";
 			cin >> tower;
 			try {
@@ -110,17 +144,31 @@ int main() {
 					break;
 				default:
 					cout << "Choose a valid tower" << endl;
-					continue; // restart the loop
+					invalidTower = true; // restart the loop
 					break;
 				}
-				fromTower->top();
+				// Make sure the player's disk is being moved
+				if (!fromTower->isEmpty()) {
+					if ((fromTower->top()->color) != (playerColors[currentPlayer])) {
+						cout << "You cannot move another player's disks. Please choose another tower." << endl;
+						invalidTower=true;
+					}
+				}
+				else { // Makes sure the tower has a disk to move
+					cout << "That tower is empty. Choose another tower." << endl;
+					invalidTower=true;
+				}				
 			}
 			catch (Stack<int>::StackUnderflow) { // if the tower is empty, tell the user
 				cout << "This tower is empty, there are no disks to pick up" << endl;
-				continue;
+				invalidTower=true;
 			}
+		}while (invalidTower);
 
-			// Put down the disk
+		// Put down the disk
+		
+		do {
+			invalidTower = false;
 			cout << "Choose the tower you would like to place the disk: ";
 			cin >> tower;
 			switch (tower) {
@@ -135,40 +183,52 @@ int main() {
 				break;
 			default:
 				cout << "Choose a valid tower" << endl;
-				continue;
+				invalidTower = true;
 				break;
 			}
+			if (toTower->isFull() || (toTower == fromTower)) { // Checks for valid moves so that the disk is not moved to the same tower or to a full tower
+				cout << "Please choose a valid tower that is not full and is not the same as the earlier tower." << endl;
+				invalidTower = true; 
+			}
+		} while (invalidTower);
 
-
-			if (toTower == fromTower) { // if you put the disk on the same tower it was picked up from
-				cout << "It's not really a move to pick up the disk and put it down in the same spot" << endl;
-			}
-			else if (toTower->isEmpty()) { // if toTower is empty, put the disk there
+		// Moves disk
+		if (toTower->isEmpty()) { // if toTower is empty, put the disk there
+			toTower->push(fromTower->pop());
+			// TODO Task 4: log move
+		}
+		else if (fromTower->top()->diskNum > toTower->top()->diskNum) {
+			cout << "That's an illegal move. You have lost your turn." << endl;
+		}
+		else { // the last scenario is when you take a disk from a tower and put it on a non empty tower
+			toTower->push(fromTower->pop());
+			// TODO Task 4: log move
+		}
+		
+		// Checks for win if the player's winning tower is full
+		if ( winningTowers[currentPlayer]->isFull() ) {
+			bool won = true;
+			for (int i = 0; i < numDisks; i++) { // Move disks to another tower to check if they all have the same color
+				fromTower = winningTowers[currentPlayer];
+				toTower = &possibleWinTower;
 				toTower->push(fromTower->pop());
-				// TODO Task 4: log move
+				cout << toTower->top()->color << " " << toTower->top()->diskNum << endl;
+				if ((toTower->top()->color) != (playerColors[currentPlayer])) { won = false; } // Set flag for won false if the disks in the tower do not have the same color
 			}
-			else if (fromTower->top() > toTower->top()) {
-				cout << "That's an illegal move" << endl;
-			}
-			else { // the last scenario is when you take a disk from a tower and put it on a non empty tower
+			for (int i = 0; i < numDisks; i++) { // Move the disks back to the original tower after checking
+				fromTower = &possibleWinTower;
+				toTower = winningTowers[currentPlayer];
 				toTower->push(fromTower->pop());
-				// TODO Task 4: log move
 			}
-			if (lTower.isFull() || rTower.isFull()) {
-				if ((lTower.top()->color.length() == 3) || (rTower.top()->color.length() == 4)) {
-					cout << "A tower is full" << endl;
-					stillPlaying = false;
-				}
-			}
-		} while (!(tower != 0 || tower != 2 || tower != 2) && toTower == fromTower);
-	}
-	string winner;
-	if (lTower.isFull()) {
-		winner = "Red";
-	}
-	else if (rTower.isFull()) {
-		winner = "Blue";
-	}
+			if (won) { // If the player has won, set the winner and exit
+				winner = playerColors[currentPlayer];
+				stillPlaying = false;				
+			}					
+		}
+		currentPlayer = changePlayer(currentPlayer); // Change the current player
+	} while (stillPlaying);
+	int a;
 	cout << "Congratulations " << winner << " has won!" << endl;
 	displayTowers(lTower, mTower, rTower, numDisks);
+	cin >> a;
 }
